60490e9a9ee3e7e834a0ef83a37cdc65320c6d16
Zhaolun Yin <yinzhaolun2012@gmail.com>
2026-08-27 08:16:51 +0100

    converted board.c to macros

--- include/ms/board.h
+++ include/ms/board.h
@@ -5,12 +5,26 @@
 #define WIDTH_MUL 2
 #define TRIM_SPACE -1
 
-int width_to_board_width(int width);
-int height_to_board_height(int height);
-int board_width_to_width(int board_width);
-int board_height_to_height(int board_height);
+#define width_to_board_width(width) \
+    (((width) * WIDTH_MUL) + BOX_WIDTH + TRIM_SPACE)
 
-int x_to_board_x(int x);
-int y_to_board_y(int y);
-int board_x_to_x(int board_x);
-int board_y_to_y(int board_y);
+#define height_to_board_height(height) \
+    ((height) + BOX_WIDTH + STATS_ROW)
+
+#define board_width_to_width(board_width) \
+    ((board_width) - BOX_WIDTH - TRIM_SPACE) / WIDTH_MUL
+
+#define board_height_to_height(board_height) \
+    ((board_height) - BOX_WIDTH - STATS_ROW)
+
+#define x_to_board_x(x) \
+    (((x) * WIDTH_MUL) + BOX_WIDTH / 2)
+
+#define y_to_board_y(y) \
+    ((y) + BOX_WIDTH / 2 + STATS_ROW)
+
+#define board_x_to_x(board_x) \
+    ((board_x) - BOX_WIDTH / 2) / WIDTH_MUL
+
+#define board_y_to_y(board_y) \
+    ((board_y) - STATS_ROW - BOX_WIDTH / 2)
